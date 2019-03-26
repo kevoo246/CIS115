6038aeac931ee9265cdee537c6fd8f7001ce2f06
kevoo246 <48959135+kevoo246@users.noreply.github.com>
2019-03-26 12:20:59 -0400

    Add files via upload

---
+++ M5T2_Fisher/src/m5t2_fisher/M5T2_Fisher.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m5t2_fisher;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fisherk6930
+ */
+public class M5T2_Fisher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // This program will say hello to the user
+        System.out.println("Hello!");
+        
+        // then ask for their name 
+        String name; 
+        Scanner keyb = new Scanner (System.in);
+        System.out.println("what's your name ?");
+        name= keyb.next();
+        
+        // then ask for their age 
+        System.out.println("How old are you ?" );
+        int age =keyb.nextInt();
+        
+        // finally, it will repeat those back
+        System.out.println("nice to meet you " + name);
+        System.out.println("i'm much younger than " + age);
+    }
+    
+}
